6c5a81c3ecf81fc873aa91e24774316deef844bf
Soravit Polpraprut <ten7009@gmail.com>
2025-04-23 13:13:02 +0700

    Fixed minor bug in app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const dayOfTheWeek = (date = new Date()) => {
 }
 
 const monthOfTheYear = ( date = new Date() ) => {
-    const months = ['January' , 'Feburary', 'March' , 'April' , 'May' , 'June' , 'July' , 'August', 'September' , 'October',
+    const month = ['January' , 'Feburary', 'March' , 'April' , 'May' , 'June' , 'July' , 'August', 'September' , 'October',
         'November', 'December'
     ]
 
